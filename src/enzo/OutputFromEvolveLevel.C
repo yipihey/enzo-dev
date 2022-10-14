ee64c69faf3e68a564970b18297929e4661af5d3
Tom Abel <tabel@stanford.edu>
2022-10-14 10:13:13 -0700

    fixup! Also output time with maximum current density

--- src/enzo/OutputFromEvolveLevel.C
+++ src/enzo/OutputFromEvolveLevel.C
@@ -117,14 +117,14 @@ int OutputFromEvolveLevel(LevelHierarchyEntry *LevelArray[],TopGridData *MetaDat
       /* Make sure we are all synced up across processors. */
       CurrentMaximumDensity = CommunicationMaxValue(CurrentMaximumDensity);
       if (MyProcessorNumber == ROOT_PROCESSOR) {
-        fprintf(stderr, "Current (time="PSYM") maximum density is %"GSYM" g/cm^3.\n",
+        fprintf(stderr, "Current (time=%"PSYM") maximum density is %"GSYM" g/cm^3.\n",
                 (Time*TimeUnits), (CurrentMaximumDensity*DensityUnits));
       }
 
       if (StopFirstTimeAtMetalEnrichedDensity > 0.) {
         CurrentMaximumMetalEnrichedDensity = CommunicationMaxValue(CurrentMaximumMetalEnrichedDensity);
         if (MyProcessorNumber == ROOT_PROCESSOR) {
-          fprintf(stderr, "Current (time="PSYM") maximum metal enriched density is %"GSYM" g/cm^3.\n",
+          fprintf(stderr, "Current (time=%"PSYM") maximum metal enriched density is %"GSYM" g/cm^3.\n",
                   (Time*TimeUnits), (CurrentMaximumMetalEnrichedDensity*DensityUnits));
         }
       }
